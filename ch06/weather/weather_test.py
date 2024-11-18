389ae632cf81d1f04d08c6f924db34e36127c136
getinthere <getinthere@naver.com>
2024-11-18 17:03:50 +0900

    ch06 weather_test 수정

--- ch06/weather/weather_test.py
+++ ch06/weather/weather_test.py
@@ -1,40 +1,30 @@
-from urllib.parse import urlencode, unquote
+from urllib.parse import unquote
 import requests
 import json
 
-url = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib"
-queryString = "?" + urlencode(
-    {
-        "ServiceKey" : unquote("wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D"), 
-        "base_date" : "20190407", 
-        "base_time" : "1200", 
-        "nx" : "59", 
-        "ny" : "127", 
-        "numOfRows" : "10", 
-        "pageNo" : "1", 
-        "_type" : "json" 
-    }
-)
+url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
+params ={
+    'serviceKey' : unquote('일반인증키'), 
+    'pageNo' : '1', 
+    'numOfRows' : '1000', 
+    'dataType' : 'JSON', 
+    'base_date' : '20240824', 
+    'base_time' : '0600', 
+    'nx' : "59", 
+    'ny' : "127" 
+}
 
-response = requests.get(url + queryString)
-print("===== response json data start =====")
-print(response)
-print("===== response json data end =====")
-print()
-
+response = requests.get(url, params=params)
 r_dict = json.loads(response.text)
 r_response = r_dict.get("response")
 r_body = r_response.get("body")
 r_items = r_body.get("items")
 r_item = r_items.get("item")
-
 result = {}
 for item in r_item:
-    if(item.get("category") == "T1H"):
-        result = item
-        break
-        
-print("===== response dictionary(python object) data start =====")
+   if(item.get("category") == "T1H"):
+       result=item
+       break
+print("============= data start ==================")
 print(result)
-print("===== response dictionary(python object) end =====")
-print()
+print("============= data start ==================")
